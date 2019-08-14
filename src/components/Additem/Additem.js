982160f986a915c946e207727774118bae1e070e
Ben Ali Tarek <benalitarek90@gmail.com>
2019-08-14 11:06:39 +0100

    Create new components

---
+++ src/components/Additem/Additem.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Additem.css'
+
+export default class Additem extends Component {
+    render() {
+        return (
+            <div className="style2">
+                AddItems Page
+            </div>
+        )
+    }
+}
---
+++ src/components/Todoitem/Todoitem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Todoitem.css'
+
+const Todoitem = () => {
+    return (
+        <div className="style1">
+            TodoItems Page
+        </div>
+    )
+}
+
+export default Todoitem
