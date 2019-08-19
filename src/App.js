f93b0c8522ec8cf0a142e958935d7952b2b9ea73
Ben Ali Tarek <benalitarek90@gmail.com>
2019-08-19 09:55:24 +0100

    Add No More Items function

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import Todoitem from './components/Todoitem/Todoitem'
 
 
 export default class App extends Component {
+
   state = {
     items :[
       {id: 1, name:'aaa', age:11},
       {id: 2, name:'bbb', age:22},
       {id: 3, name:'ccc', age:33}
     ]
-  }
+  };
+
+
 
   deleteItem = (id ) => {
     const it = this.state.items.filter(item =>{
@@ -21,7 +24,7 @@ export default class App extends Component {
     this.setState({
       items:it
     })
-    console.log("Id : " + id + "  is deleted")
+    console.log( ` Id : ${id} is deleted `)
 
   }
   render() {
--- src/components/Todoitem/Todoitem.js
+++ src/components/Todoitem/Todoitem.js
@@ -4,19 +4,30 @@ import "./Todoitem.css";
 // Stateless Components
 const Todoitem = (props) => {
   const { items, deleteItem } = props;
-  const listitems = items.map(item => {
-    return (
-      <div key={item.id}>
-        <span>{item.name}</span>
-        <span>{item.age}</span>
-        <span onClick={() => deleteItem(item.id)}>&times;</span>
-      </div>
-    );
-  });
+
+  const listitems = items.length ? (
+    items.map(item => {
+      return (
+        <div key={item.id}>
+          <span>{item.name}</span>
+          <span>{item.age}</span>
+          <span onClick={() => deleteItem(item.id)}>&times;</span>
+        </div>
+      )
+    })
+  )
+    : (
+      <p>No More Items</p>
+    )
+
+
+
+
+
   return (
-  <div>
-  {listitems}
-  </div>
+    <div>
+      {listitems}
+    </div>
   )
 };
 
